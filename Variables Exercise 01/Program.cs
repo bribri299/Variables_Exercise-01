b0f7fdcd2f84c5b543dc3b90e212246c245a7d53
Brittany McClellan <brittanyhines36@yahoo.com>
2022-03-24 22:25:27 -0500

    TODOS Complete

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 
 namespace Variables_Exercise_01
 {
@@ -19,6 +19,12 @@ namespace Variables_Exercise_01
             // double
             // decimal
             #endregion
+            string myName = "Brittany";
+            char midInitial = 'J';
+            bool isMarried = true;
+            int myAge = 25;
+            double myWeight = 165.33;
+            decimal myAccount = 500.25m;
 
             // TODO: Declare a variable for each of the types above
             // Make sure to use C# naming convention best practices 
@@ -26,6 +32,8 @@ namespace Variables_Exercise_01
             // TODO: Initialize each of the variables with a value
 
             // TODO: Write out each of the variables to the Console 
+            Console.WriteLine($"Hello my name is {myName}, I am {myAge} years old.");
+            Console.WriteLine($"Its {isMarried} I'm married with {myAccount} in the bank");
         }
 
     }
